9cf0a8f8dd73227f4fc06fde231da8a7c9349f8b
JeanG1914 <jeanpaulgzea@gmail.com>
2022-12-10 01:48:27 +0000

    beginin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() {};
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+
+  for(let i)
+ 
+  console.log("Any string");
