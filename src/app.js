4ec002ae2ac32fd35a08a68b8d611a549bd18bcc
JeanG1914 <jeanpaulgzea@gmail.com>
2022-12-14 22:35:22 +0000

    progress

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {};
-  let pronoun = ['the','our'];
-  let adj = ['great', 'big' ];
-  let noun = ['jogger','racoon'];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-  for(let i)
- 
-  console.log("Any string");
+for (let i = pronoun; i == pronoun; i++) {
+  for (let k = adj; k != i; k++) {
+    for (let o = noun; o != i; o++)
+  }
+}
